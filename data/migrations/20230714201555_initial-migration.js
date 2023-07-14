350d54673b839162b966205d3c4199e3856a3577
Michael Newell <michael.newell212@gmail.com>
2023-07-14 14:36:56 -0600

    wip

---
+++ data/migrations/20230714201555_initial-migration.js
@@ -0,0 +1,63 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.schema
+    .createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name', 200).notNullable().unique()
+        table.boolean('project_completed')
+    })
+    .createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('resource_name')
+    })
+    .createTable('tasks', table => {
+        table.increments('task_id')
+        table.string('task_desription')
+        table.boolean('task_completed')
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+    })
+    .createTable('project_resources', table => {
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        table.string('project_name')
+            .unsigned()
+            .notNullable()
+            .references('project_name')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        table.string('resource_name')
+            .unsigned()
+            .notNullable()
+            .references('resource_name')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
+};
