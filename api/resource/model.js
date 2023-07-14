975957bad1245ef201dc65a7d555277e3cadb6f5
Michael Newell <michael.newell212@gmail.com>
2023-07-14 12:08:13 -0600

    wip

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,5 @@
-// build your `Resource` model here
+function getResource() {
+    return Promise.resolve('Resources retrieved')
+}
+
+module.exports = { getResource }
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const Resource = require('./model')
 
+router.get('/', (req, res, next) => {
+    Resource.getResource()
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(next)
+})
+
 router.use((err, req, res, next) => { // eslint-disable-line
     res.status(500).json({
         customMessage: 'something went wrong in the resources router',
