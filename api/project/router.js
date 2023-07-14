05edf833f1de2a5a81f3696331d2cf0388e5a6f9
Michael Newell <michael.newell212@gmail.com>
2023-07-14 11:51:34 -0600

    wip

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,12 @@
-// build your `/api/projects` router here
+const router = require('express').Router()
+const Project = require('./model')
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customMessage: 'something went wrong in the project router',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,13 @@
-// build your `/api/tasks` router here
+const router = require('express').Router()
+const Task = require('./model')
+
+router.use((err, req, res, next) => {
+    res.status(500).json({
+        customMessage: 'somthing went wrong in the task router',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+
+module.exports = router
